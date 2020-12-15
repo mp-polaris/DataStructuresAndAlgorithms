47456f22af6fd5df3ba09dbf7d323b06319ec163
polaris <polaris424@foxmail.com>
2020-12-15 21:01:18 +0800

    first commit

---
+++ src/main/java/com/polaris/Main.java
@@ -0,0 +1,51 @@
+package com.polaris;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("gg");
+        System.out.println("gg");
+        System.out.println("gg");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
